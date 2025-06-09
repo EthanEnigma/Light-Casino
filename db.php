3f610c80a8c5f5e04bf314aaf9079a02b1219c08
EthanEnigma <Ethan.LEGRIS@enigma-school.com>
2025-06-09 16:17:58 +0200

    fix2

--- db.php
+++ db.php
@@ -1,14 +1,9 @@
 <?php
-require_once __DIR__ . '/vendor/autoload.php'; // pour charger phpdotenv
-
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-
 try {
     $pdo = new PDO(
-        "mysql:host=" . $_ENV['DB_HOST'] . ";port=" . $_ENV['DB_PORT'] . ";dbname=" . $_ENV['DB_NAME'] . ";charset=utf8mb4",
-        $_ENV['DB_USER'],
-        $_ENV['DB_PASS']
+        "mysql:host=" . getenv('DB_HOST') . ";port=" . getenv('DB_PORT') . ";dbname=" . getenv('DB_NAME') . ";charset=utf8mb4",
+        getenv('DB_USER'),
+        getenv('DB_PASS')
     );
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
