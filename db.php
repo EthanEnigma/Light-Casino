148a36b6203a76c8f3933f2fc8e329226acd6c51
EthanEnigma <Ethan.LEGRIS@enigma-school.com>
2025-06-09 15:55:21 +0200

    fix: delete config.php, fix db.php

--- db.php
+++ db.php
@@ -1,5 +1,9 @@
 <?php
-require_once 'config.php';
+define('DB_HOST', getenv('DB_HOST'));
+define('DB_PORT', getenv('DB_PORT'));
+define('DB_NAME', getenv('DB_NAME'));
+define('DB_USER', getenv('DB_USER'));
+define('DB_PASS', getenv('DB_PASS'));
 
 try {
     $pdo = new PDO(
